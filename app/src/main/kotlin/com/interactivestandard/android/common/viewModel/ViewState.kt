16e86c9e441fd899e2080fd96d37e4bd2a29d0cc
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2023-02-01 22:22:41 +0300

    Clear © copyright.

--- app/src/main/kotlin/com/interactivestandard/android/common/viewModel/ViewState.kt
+++ app/src/main/kotlin/com/interactivestandard/android/common/viewModel/ViewState.kt
@@ -1,7 +1,3 @@
 package com.interactivestandard.android.common.viewModel
 
-/**
- * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
- * for InteractiveStandard on 24.01.2023 16:39.
- */
 interface ViewState
